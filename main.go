4a544822e82077e721393b3fd110ccd7d8353757
Matthias Loibl <mail@matthiasloibl.com>
2019-11-01 17:57:57 +0100

    Accept errorbudget as number 0-100

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/ghodss/yaml"
 	"github.com/go-chi/chi"
@@ -17,8 +18,8 @@ func main() {
 	})
 
 	r := chi.NewRouter()
-	r.Get("/", foo(index))
-	r.Post("/generate", foo(generate(vm)))
+	r.Get("/", HandleFunc(index))
+	r.Post("/generate", HandleFunc(generate(vm)))
 
 	if err := http.ListenAndServe(":9099", r); err != nil {
 		log.Println(err)
@@ -47,7 +48,7 @@ var indexTemplate = `
 		<input type="text" placeholder='[job="prometheus"]' required id="selectors" name="selectors">
 		<br>
 		<label for="errorbudget">ErrorBudget:</label>
-		<input type="text" placeholder='1-0.99' required id="errorbudget" name="errorbudget">
+		<input type="text" placeholder='99.9' required id="errorbudget" name="errorbudget">
 		<br><br>
 		<button type="submit">Generate</button>
 	</form>
@@ -73,23 +74,30 @@ local slo = import 'slo-libsonnet/slo.libsonnet';
     metric: '%s',
     selectors: %s,
 
-    errorBudget: %s,
+    errorBudget: %f,
   }),
 
   // Output these as example
   recordingrule: errorburnrate.recordingrules,
   alerts: errorburnrate.alerts,
 }
-
 `
 
 func generate(vm *jsonnet.VM) HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) (int, error) {
 		metric := r.FormValue("metric")
 		selectors := r.FormValue("selectors")
-		errorbudget := r.FormValue("errorbudget")
+		formErrorBudget := r.FormValue("errorbudget")
 
-		snippet := fmt.Sprintf(errorBurnRate, metric, selectors, errorbudget)
+		errorBudget, err := strconv.ParseFloat(formErrorBudget, 64)
+		if err != nil {
+			return http.StatusUnprocessableEntity, fmt.Errorf("formErrorBudget is not a float: %w", err)
+		}
+		if errorBudget < 0 || errorBudget > 100 {
+			return http.StatusUnprocessableEntity, fmt.Errorf("errorBudget has to be between 0 and 100")
+		}
+
+		snippet := fmt.Sprintf(errorBurnRate, metric, selectors, 100-errorBudget)
 		json, err := vm.EvaluateSnippet("", snippet)
 		if err != nil {
 			return http.StatusInternalServerError, err
@@ -109,7 +117,7 @@ func generate(vm *jsonnet.VM) HandlerFunc {
 
 type HandlerFunc func(http.ResponseWriter, *http.Request) (int, error)
 
-func foo(h HandlerFunc) http.HandlerFunc {
+func HandleFunc(h HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		statusCode, err := h(w, r)
 		if err != nil {
